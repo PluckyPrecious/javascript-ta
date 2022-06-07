0b9d221579bf66667d0ef0a49da0dfba71af3f92
PluckyPrecious <prekel27@gmail.com>
2022-06-07 11:01:22 +0100

    :heavy_plus_sign: Add function
    A function that checks if a number is prime or not and returns bool.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 // complete the function
 function prime (num) {
-  // code goes here
+  var sqrtnum=Math.floor(Math.sqrt(num));
+  var prime = num != 1;
+  for(var i=2; i<sqrtnum+1; i++) { // sqrtnum+1
+      if(num % i == 0) {
+          prime = false;
+          break;
+      }
+  }
+  return prime;
 }
 
 function solution (arg) {
